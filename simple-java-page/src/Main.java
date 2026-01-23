cbd58c9852a29c5e2b1c6f0066663c44f723eb26
Rupeshs11 <rbsatale7@gmail.com>
2026-01-23 17:42:13 +0530

    Docker praticals

--- simple-java-page/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Main {
-    public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println("Hello, Knox! Current date: " + currentDate);
-	System.out.println("Welcome to the container");
-    }
-}
